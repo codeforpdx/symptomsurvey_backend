930c93194d6255e85407f63f7f35c8031891b88c
GerryFudd <davehasen@gmail.com>
2018-12-10 22:02:15 -0800

    passwords are hidden by a salt

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import flask
 import json
 import flask_pymongo
+import hashlib
+import binascii
+from pprint import pprint
+
+with open('constants.json') as f:
+    salt_constants = json.load(f)
+
+def make_hash(password, salt):
+  dk = hashlib.pbkdf2_hmac(salt_constants['salt_algorithm'], bytes(password, 'utf-8'), bytes(salt, 'utf-8'), int(salt_constants['salt_iterations']), dklen=int(salt_constants['salt_key_length']))
+  print(binascii.hexlify(dk))
+  return binascii.hexlify(dk)
 
 # configure flask app
 app = flask.Flask(__name__)
@@ -17,7 +28,7 @@ def login():
   password = flask.request.form['password']
   for user in mongo.db.users.find():
     # TODO: use a salt for passwords so they aren't persisted naked.
-    if ((username == user['profile']['username']) and (password == user['password'])):
+    if ((username == user['profile']['username']) and (make_hash(password, user['password']['salt']) == bytes(user['password']['hash'], 'utf-8'))):
       return json.dumps({'profile': user['profile'], 'role': user['role']})
 
   return json.dumps({'error': 'There is no user with this username and password.'}), 400
--- migrations/20181211035401-populate_users.js
+++ migrations/20181211035401-populate_users.js
@@ -1,8 +1,28 @@
+const {pbkdf2} = require('crypto');
+const constants = require('../constants');
+const {salt_iterations, salt_key_length, salt_algorithm} = constants;
+
+const USER_PASSWORD = 'pw';
+const USER_SALT = '209880c8-df7d-4792-8eb1-08092d03f1b0';
+
+const ADMINISTRATOR_PASSWORD = 'superC0Mp13x';
+const ADMINISTRATOR_SALT = '0f3e8d68-13a0-4cef-b9f1-359862d7566a';
+
+const makeHash = (password, salt) => new Promise((resolve, reject) => pbkdf2(password, salt, Number(salt_iterations), Number(salt_key_length), salt_algorithm, (err, result) => {
+  if (err) {
+    reject(err);
+    return;
+  }
+  resolve(result.toString('hex'));
+}));
+
 module.exports = {
   up(db) {
-    return db
-    .createCollection('users')
-    .then(() => {
+    return Promise.all([
+      makeHash(USER_PASSWORD, USER_SALT),
+      makeHash(ADMINISTRATOR_PASSWORD, ADMINISTRATOR_SALT),
+      db.createCollection('users')
+    ]).then(([userHash, administratorHash]) => {
       db.collection('users').insertMany([
         {
           profile: {
@@ -10,7 +30,10 @@ module.exports = {
             firstName: 'Robert',
             lastName: 'Smith'
           },
-          password: 'pw',
+          password: {
+            salt: USER_SALT,
+            hash: userHash
+          },
           role: 'user'
         },
         {
@@ -19,7 +42,10 @@ module.exports = {
           firstName: 'Reginald',
           lastName: 'McFancypants'
          },
-         password: 'superC0Mp13x',
+         password: {
+           salt: ADMINISTRATOR_SALT,
+           hash: administratorHash
+         },
          role: 'administrator'
         }
       ]);
