38376f48ab0f15b4f82d1d827106b9ee80485a9d
GerryFudd <davehasen@gmail.com>
2018-12-11 12:38:02 -0800

    removed unused import and added comments

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ import flask_pymongo
 import hashlib
 import binascii
 import jwt
-from pprint import pprint
 
+# Load the values from the constants file.  This file contains the parameters that are used for the
+# hashing algorithim that is applied to the salted passwords.
 with open('constants.json') as f:
     salt_constants = json.load(f)
 
+# Read the private key from `./keys/token`. This file is gitignored so that our private key is not
+# checked into version control.
 with open('keys/token') as key:
     private_key = key.read()
 
-with open('keys/token.pub') as key:
-    public_key = key
-
+# Passwords will not be stored in the database.  Instead they will be encoded using a common
+# hashing algorithm and a "salt". Each user will have their own salt, which will be a uuid V4.
+# This algorithm will be applied when the user sets their password and when they log in to compare
+# the hash result to the hash of the password that's stored in the database.
 def make_hash(password, salt):
   dk = hashlib.pbkdf2_hmac(salt_constants['salt_algorithm'], bytes(password, 'utf-8'), bytes(salt, 'utf-8'), int(salt_constants['salt_iterations']), dklen=int(salt_constants['salt_key_length']))
   print(binascii.hexlify(dk))
@@ -23,18 +27,25 @@ def make_hash(password, salt):
 # configure flask app
 app = flask.Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://127.0.0.1:27017/social_media_scraper'
+# TODO: add a password to the mongo database so that it's contents are hidden behind this API
+
+# Create a mongo client that works with the flask configuration.
 mongo = flask_pymongo.PyMongo(app)
 
+# Example route
+# TODO: remove this endpoint before this app goes into production
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+# Login endpoint.  Checks a submitted username and password against the users in the users collection.
+# If there is a match, the user's profile and role are put into a Json Web Token and that token is
+# returned.
 @app.route('/login', methods=['POST'])
 def login():
   username = flask.request.form['username']
   password = flask.request.form['password']
   for user in mongo.db.users.find():
-    # TODO: use a salt for passwords so they aren't persisted naked.
     if ((username == user['profile']['username']) and (make_hash(password, user['password']['salt']) == bytes(user['password']['hash'], 'utf-8'))):
       token = jwt.encode({'profile': user['profile'], 'role': user['role']}, private_key, algorithm='RS256')
       return json.dumps({'token': token.decode('utf-8')})
--- migrations/20181211035401-populate_users.js
+++ migrations/20181211035401-populate_users.js
@@ -1,13 +1,19 @@
 const {pbkdf2} = require('crypto');
-const constants = require('../constants');
-const {salt_iterations, salt_key_length, salt_algorithm} = constants;
+const {salt_iterations, salt_key_length, salt_algorithm} = require('../constants');
 
+// Pre configured values for initial users' passwords and uuid V4 salts.
 const USER_PASSWORD = 'pw';
 const USER_SALT = '209880c8-df7d-4792-8eb1-08092d03f1b0';
 
 const ADMINISTRATOR_PASSWORD = 'superC0Mp13x';
 const ADMINISTRATOR_SALT = '0f3e8d68-13a0-4cef-b9f1-359862d7566a';
 
+/**
+ * makeHash
+ * @param {string} password - The user's actual password.
+ * @param {string} salt - The randomly generated salt.
+ * @returns {Promise<String>} A promise that resolves to a hex string of the password hash
+ */
 const makeHash = (password, salt) => new Promise((resolve, reject) => pbkdf2(password, salt, Number(salt_iterations), Number(salt_key_length), salt_algorithm, (err, result) => {
   if (err) {
     reject(err);
@@ -18,11 +24,13 @@ const makeHash = (password, salt) => new Promise((resolve, reject) => pbkdf2(pas
 
 module.exports = {
   up(db) {
+    // Generate both password hashes and create the users collection.
     return Promise.all([
       makeHash(USER_PASSWORD, USER_SALT),
       makeHash(ADMINISTRATOR_PASSWORD, ADMINISTRATOR_SALT),
       db.createCollection('users')
     ]).then(([userHash, administratorHash]) => {
+      // Insert both users with the appropriate salt/passwordHash values.
       db.collection('users').insertMany([
         {
           profile: {
@@ -53,6 +61,7 @@ module.exports = {
   },
 
   down(db) {
+    // Running this down script will destroy all data in the users table.
     return db.collection('users').drop();
   }
 };
