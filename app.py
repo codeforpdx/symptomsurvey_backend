11c587fe7d83dd5764b1848df8632d1675d8cb26
GerryFudd <davehasen@gmail.com>
2018-12-10 22:27:09 -0800

    jwt served up rather than naked json

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ import json
 import flask_pymongo
 import hashlib
 import binascii
+import jwt
 from pprint import pprint
 
 with open('constants.json') as f:
     salt_constants = json.load(f)
+
+with open('keys/token') as key:
+    private_key = key.read()
+
+with open('keys/token.pub') as key:
+    public_key = key
 
 def make_hash(password, salt):
   dk = hashlib.pbkdf2_hmac(salt_constants['salt_algorithm'], bytes(password, 'utf-8'), bytes(salt, 'utf-8'), int(salt_constants['salt_iterations']), dklen=int(salt_constants['salt_key_length']))
@@ -29,6 +36,7 @@ def login():
   for user in mongo.db.users.find():
     # TODO: use a salt for passwords so they aren't persisted naked.
     if ((username == user['profile']['username']) and (make_hash(password, user['password']['salt']) == bytes(user['password']['hash'], 'utf-8'))):
-      return json.dumps({'profile': user['profile'], 'role': user['role']})
+      token = jwt.encode({'profile': user['profile'], 'role': user['role']}, private_key, algorithm='RS256')
+      return json.dumps({'token': token.decode('utf-8')})
 
   return json.dumps({'error': 'There is no user with this username and password.'}), 400
