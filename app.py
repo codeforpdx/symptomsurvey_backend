fe6b65af804991186e36f89cf646e20f81786871
GerryFudd <davehasen@gmail.com>
2018-12-11 20:15:58 -0800

    I can't believe I had that typo

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def make_hash(password, salt):
 app = flask.Flask(__name__)
 flask_cors.CORS(app)
 app.config['MONGO_URI'] = 'mongodb://127.0.0.1:27017/social_media_scraper'
-# TODO: add a password to the mongo database so that it's contents are hidden behind this API
+# TODO: add a password to the mongo database so that its contents are hidden behind this API
 
 # Create a mongo client that works with the flask configuration.
 mongo = flask_pymongo.PyMongo(app)
