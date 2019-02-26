a38b4725bd79515a4b27d7dd8505629b2be16b96
marty-hope <marty.hope@gmail.com>
2019-02-26 12:25:22 -0800

    Add flasgger Update with app info Add default and login routes documentation

--- WEB/controllers/session.py
+++ WEB/controllers/session.py
@@ -10,6 +10,50 @@ def add_routes(app):
   # returned.
   @app.route('/login', methods=['POST'])
   def login():
+    '''
+    Login endpoint.  Checks a submitted username and password against the users in the users collection.
+    If there is a match, the user's profile and role are put into a Json Web Token and that token is
+    returned.
+    ---
+    definitions:
+      Login:
+        type : object
+        properties:          
+          password:
+            type: string
+          username:
+            type: string
+      Token:
+        type: object
+        properties:
+          token:
+            type: string
+      Error:
+        type: object
+        properties:
+          error:
+            type: string
+    parameters:
+        - name: login
+          in: body
+          type: object
+          required: true
+          schema:
+            $ref: '#/definitions/Login'
+    responses:
+      200:
+        description: Successfully authenticated
+        schema:
+          $ref: '#/definitions/Token'
+        examples: 
+          token: '*elided_token_value*'
+      401:
+        description: Authentication failed
+        schema:
+          $ref: '#/definitions/Error'
+        examples:
+          error: 'There is no user with this username and password.'      
+    '''
     body = flask.request.get_json()
     username = body['username']
     password = body['password']
@@ -18,4 +62,4 @@ def add_routes(app):
     if result['valid']:
       return json.dumps({'token': result['token']}), 200, {'Content-Type': 'application/json'}
 
-    return json.dumps({'error': 'There is no user with this username and password.'}), 400
+    return json.dumps({'error': 'There is no user with this username and password.'}), 401
