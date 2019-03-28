bb317fc1529c1bf6a31868a5c4fb4693e77f2ec3
GerryFudd <davehasen@gmail.com>
2019-03-27 19:11:47 -0700

    more progress

--- WEB/controllers/tweets.py
+++ WEB/controllers/tweets.py
@@ -4,8 +4,11 @@ from services import auth_service, tweets_service
 
 def add_routes(app):
   @app.route('/tweets/load', endpoint='load_tweets')
-  @auth_service.require_permission([])
+  # @auth_service.require_permission([])
   def load_tweets():
-    tweets = json.dumps(tweets_service.get_tweets_from_twitter())
-    tweets_service.save_tweets(tweets)
-    return tweets, 200, {'Content-Type': 'application/json'}
+    tweets = tweets_service.get_tweets_from_twitter()
+    try:
+      tweets_service.save_tweets(tweets)
+    except Exception as ex:
+      return json.dumps(ex), 500, {'Content-Type': 'application/json'}
+    return json.dumps(tweets), 200, {'Content-Type': 'application/json'}
