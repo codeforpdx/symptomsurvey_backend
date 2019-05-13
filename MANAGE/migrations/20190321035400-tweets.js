024b7cc1f53e6d1ccd0cd367ceb58ec600f02e0c
GerryFudd <davehasen@gmail.com>
2019-05-12 20:43:49 -0700

    added indexes for text and geo

--- MANAGE/migrations/20190321035400-tweets.js
+++ MANAGE/migrations/20190321035400-tweets.js
@@ -1,6 +1,12 @@
+const tweets = require('../seeds/tweets');
+
 module.exports = {
   up(db) {
-    return db.createCollection('tweets');
+    return db
+      .createCollection('tweets')
+      .then(() => db.collection('tweets').insertMany(tweets))
+      .then(() => db.collection('tweets').createIndex({text: 'text'}))
+      .then(() => db.collection('tweets').createIndex({geo: '2dsphere'}));
   },
 
   down(db) {
