c7eb54ea53508ee6d2a1d4ba5103f511ec328248
Megan Porter <mossytreesandferns@gmail.com>
2019-05-20 18:00:47 -0700

    Recent changes to CFPDXTweetGrab.py

---
+++ WEB/tests/CFPDXTweetGrab.py
@@ -0,0 +1,42 @@
+
+
+import json
+import time
+from datetime import datetime
+
+clackamas_lat_long = {
+    'Clackamas':[45.407509, -122.568848],'Oregon City':[45.357819,-122.608437],
+    'Happy Valley':[45.446960,-122.530838],'Milwaukie':[45.445530,-122.641663],'West Linn'[45.366718,-122.614441],
+    'Canby':[45.262291,-122.692162],'Estacada':[45.289688,-122.335121],'Molalla':[45.147121,-122.575317],
+    'Canby':[45.262291,-122.692162], 'Gladstone':[45.382198,-122.595901],'Boring':[45.430057,-122.373523],
+    'Damascus':[45.417625,-122.458984], 'Beaver Creek':[45.504051,-122.386925], 'Mulino':[45.221511,-122.582033],
+    'Oak Grove':[45.416239,-122.641562],'Government Camp':[45.304103,-121.754760],'Oatfield':[45.412658,-122.598009],
+    'Jennings Lodge':[45.392020,-122.614704],'Johnson City':[45.404565,-122.578981],
+    'Stafford':[45.357342,-122.722595],'Barlow':[45.252257,-122.721223],
+}
+
+
+with open("MANAGE/seeds/tweets.json", "r") as tweets_json:
+    tweet_grab = json.load(tweets_json)
+    #print(tweet_grab)
+
+for tweet in tweet_grab:
+    if tweet['location'] in clackamas_lat_long:
+        print(clackamas_lat_long[tweet['location']])    
+
+for tweet in tweet_grab:
+    timestamp = datetime.strptime(tweet['created_at'],
+                   '%a %b %d %H:%M:%S +0000 %Y')
+    #print(timestamp)
+
+for tweet in tweet_grab:
+    if tweet['coordinates'] != None:
+        coordinates =  tweet['geo']['coordinates']
+        #print(coordinates)
+
+
+for tweet in tweet_grab:
+    if tweet['place'] != None:
+        #print(tweet['place']['bounding_box']['coordinates'])
+
+
--- WEB/tests/example_model_test.py
+++ WEB/tests/example_model_test.py
@@ -6,3 +6,8 @@ def test_model_has_parameter_called_Example():
 
 def test_has_constructor_that_consumes_json():
   example_model.Example(json.dumps({}))
+
+with open('../MANAGE/seeds/users.json') as seed:
+      base_data = json.load(seed)
+      # test_app.py 
+       
