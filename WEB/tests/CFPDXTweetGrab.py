585d0b48b9b32670943c05a6c270300e23b1a849
Megan Porter <mossytreesandferns@gmail.com>
2019-05-22 19:09:24 -0700

    Removing ignore files

--- WEB/tests/CFPDXTweetGrab.py
+++ WEB/tests/CFPDXTweetGrab.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 class TweetGrab:
     # Assumption that tweet is already converted to python via json.load
+
     def __init__(self, tweet): 
         self.tweet = tweet
 
@@ -13,7 +14,7 @@ class TweetGrab:
         'Clackamas':[45.407509, -122.568848],'Oregon City':[45.357819,-122.608437],
         'Happy Valley':[45.446960,-122.530838],'Milwaukie':[45.445530,-122.641663],'West Linn'[45.366718,-122.614441],
         'Canby':[45.262291,-122.692162],'Estacada':[45.289688,-122.335121],'Molalla':[45.147121,-122.575317],
-        'Canby':[45.262291,-122.692162], 'Gladstone':[45.382198,-122.595901],'Boring':[45.430057,-122.373523],
+        'Gladstone':[45.382198,-122.595901],'Boring':[45.430057,-122.373523],
         'Damascus':[45.417625,-122.458984], 'Beaver Creek':[45.504051,-122.386925], 'Mulino':[45.221511,-122.582033],
         'Oak Grove':[45.416239,-122.641562],'Government Camp':[45.304103,-121.754760],'Oatfield':[45.412658,-122.598009],
         'Jennings Lodge':[45.392020,-122.614704],'Johnson City':[45.404565,-122.578981],
