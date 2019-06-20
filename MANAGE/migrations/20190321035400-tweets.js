044ae24dd79b969da3d1cf9e9f7ef52789890ebf
Alexander Clifford <AlexanderNull@users.noreply.github.com>
2019-06-19 20:33:36 -0700

    Adds simple search on stored data (#25)
    * Adds simple search on stored data
    
    * added indexes for text and geo
    
    * added indexes for the tweet collection
    
    * Removes extraneous index generation

--- MANAGE/migrations/20190321035400-tweets.js
+++ MANAGE/migrations/20190321035400-tweets.js
@@ -1,6 +1,9 @@
 module.exports = {
   up(db) {
-    return db.createCollection('tweets');
+    return db
+      .createCollection('tweets')
+      .then(() => db.collection('tweets').createIndex({text: 'text'}))
+      .then(() => db.collection('tweets').createIndex({geo: '2dsphere'}));
   },
 
   down(db) {
