7d9e12fffdf4891c507805572a0023cffa0a8462
Robert Lugg <robertlugg@Roberts-MBP.home>
2019-07-15 23:59:39 -0700

    swagger doc strings added to /python endpoint and /get_users

--- WEB/controllers/users.py
+++ WEB/controllers/users.py
@@ -7,4 +7,11 @@ def add_routes(app):
   @app.route('/users', endpoint='get_users')
   @auth_service.require_permission(['USER_READ'])
   def get_users():  # pylint: disable=unused-variable
+    '''
+    Retrieves registered users.
+    ---
+    responses:
+      200:
+        description: JSON Dump of users
+    '''
     return json.dumps(users_service.get_all()), 200, {'Content-Type': 'application/json'}
