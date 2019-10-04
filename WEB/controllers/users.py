2b9fa9d1457c1185237f9e7c098a44dce1ce33c0
Alexander Clifford <alexander.clifford@oracle.com>
2019-10-03 17:47:54 -0700

    Adds back auth check on user

--- WEB/controllers/users.py
+++ WEB/controllers/users.py
@@ -7,7 +7,7 @@ JSON_CONTENT_TYPE = {'Content-Type': 'application/json'}
 
 def add_routes(app):
   @app.route('/users', endpoint='get_users')
-  # @auth_service.require_permission(['USER_READ'])
+  @auth_service.require_permission(['USER_READ'])
   def get_users():  # pylint: disable=unused-variable
     '''
     Retrieves registered users.
